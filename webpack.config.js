456e6e7f2816ab46045dcd4454ba2d806f370da4
Cao Anh Quan <aquancva@gmail.com>
2018-06-29 14:06:06 +0700

    split the vendor code

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,26 @@ module.exports = (env, argvs) => {
             // new webpack.HotModuleReplacementPlugin(),
 
         ],
+
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        chunks: "initial",
+                        minChunks: 2,
+                        maxInitialRequests: 5, // The default limit is too small to showcase the effect
+                        minSize: 0 // This is example is too small to create commons chunks
+                    },
+                    vendor: {
+                        test: /node_modules/,
+                        chunks: "initial",
+                        name: "vendor",
+                        priority: 10,
+                        enforce: true
+                    }
+                }
+            }
+        },
         devServer: {
             contentBase: './dist',
             historyApiFallback: true,
